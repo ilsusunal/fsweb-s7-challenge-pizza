7c5ead915958d6aee079b85b4d862c1e7908baba
ilsu-sunal <sunalilsu@gmail.com>
2024-02-12 14:45:53 +0300

    form pizza info eklendi

--- src/components/OrderPizza.jsx
+++ src/components/OrderPizza.jsx
@@ -10,6 +10,8 @@ const initialData = {
   boyut: "",
   malzemeler: [],
   özel: "",
+  puan: "",
+  yorumlar: "",
 }
 const errorMessages = {
   checkbox1 : "En az 4 tane malzeme seçmelisiniz!",
@@ -31,6 +33,16 @@ export default function OrderPizza() {
     return (
 
         <Form className="order-pizza-form">
+          {/*Pizza Info*/}
+          <h2>Pizza Adı</h2>
+          <div>
+            <div className='pizza-info'>
+              <h1>Pizza fiyatı</h1>
+              <p>Puanı</p>
+              <p>Yorum Sayısı</p>
+            </div>
+            <p>Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir.</p> 
+          </div>
           <div className="form-group-row">
            {/* Pizza boyutu */} 
            <FormGroup >
